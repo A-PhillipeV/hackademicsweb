ac902223b0443ddb0c9840642c36def5476d88c9
dvala041 <86671843+dvala041@users.noreply.github.com>
2022-11-26 11:29:09 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__  == '__main__':
+    app.run()
+    
